b8dce4395d557f0c233d53f978801f2594230aba
SenRa User <ronakchitlangya1997@gmail.com>
2022-06-18 19:53:15 +0530

    feature: Blocked account for 3 mint after 3 attempt

--- Auth/views.py
+++ Auth/views.py
@@ -3,6 +3,7 @@ from Auth.forms import createUserForm
 from Auth.models import DRCUser
 import random
 from django.contrib.auth import authenticate, login as UserLogin, logout as UserLogout
+import datetime
 # Create your views here.
 
 # Registration page function
@@ -54,17 +55,34 @@ def VerifyOTP(request):
 
 def login(request):
     loginErrorMessage = False
+    errorMessage = False
+    datetimenow = datetime.datetime.now()
+    starttime = datetimenow.strftime("%M")
+    request.session["login_starttime"] = starttime
     Phonenumber = request.session['PhoneNumber']
     if request.method == 'POST':
         Username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, Phone_number=Phonenumber, username=Username, password=password)
         if user is not None:   
+            request.session["login_attempts"] = 1
+            request.session["login_starttime"] = starttime
             UserLogin(request, user)
             return redirect('Home') 
         else:    
             loginErrorMessage = True
-    context={'loginErrorMessage': loginErrorMessage}
+            count = 1
+            request.session["login_attempts"] += 1
+            request.session["login_endtime"] = starttime
+    
+    if request.session["login_endtime"]:
+        if int(request.session["login_endtime"])+5 >= int(request.session["login_starttime"]):
+            request.session["login_attempts"] = 1
+            request.session["login_endtime"] = starttime
+        else:
+            pass
+
+    context={'loginErrorMessage': loginErrorMessage, 'numberofattempts':request.session["login_attempts"]}
     return render(request, 'login.html', context)
 
 def logout(request):
